84806619970dc718f09fd53faed93be73ec2e048
João Adriano <95387611+joaoadriano2005@users.noreply.github.com>
2023-03-07 15:07:56 -0300

    Add files via upload

---
+++ Questões/Atividade 2/Atividade 2/main.js
@@ -0,0 +1,25 @@
+
+var resultado = 0;
+// Função para exibir o resultado na tela
+function exibirResultado() {
+  var divResultado = document.getElementById("resultado");
+  divResultado.innerHTML = "Seu IMC é: "+resultado ;
+}
+
+// Função para realizar o cálculo
+function calcular() {
+  // Lê os valores dos inputs
+  var alt = document.getElementById("input1").value;
+  var kg = document.getElementById("input2").value;
+
+  qua=alt*alt
+  resultado = qua/kg;
+
+
+  exibirResultado();
+}
+
+// Exibe o resultado na tela quando a página for carregada
+window.onload = function() {
+  exibirResultado();
+};
