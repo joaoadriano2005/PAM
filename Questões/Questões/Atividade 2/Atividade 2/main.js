582bb8da6bcd7a21c5d845c29e34e0fb976b0633
João Adriano <95387611+joaoadriano2005@users.noreply.github.com>
2023-03-14 11:48:09 -0300

    Update main.js

--- Questões/Questões/Atividade 2/Atividade 2/main.js
+++ Questões/Questões/Atividade 2/Atividade 2/main.js
@@ -1,22 +1,8 @@
 
-var resultado = 0;
+var imc = 0;
 // Função para exibir o resultado na tela
 function exibirResultado() {
-  var divResultado = document.getElementById("resultado");
-  divResultado.innerHTML = "" ;
-  function interpretarIMC(imc) {
-  // Interpreta o IMC
-  if (imc < 18.5) {
-    divResultado.innerHTML = "" ;
-  } else if (imc >= 18.5 && imc < 25) {
-     divResultado.innerHTML = "" ;
-  } else if (imc >= 25 && imc < 30) {
-     divResultado.innerHTML = "" ;
-  } else {
-    divResultado.innerHTML = "" ;
-  }
-}
-
+  interpretarIMC(imc);
 }
 
 // Função para realizar o cálculo
@@ -35,3 +21,19 @@ function calcular(kg,alt) {
 window.onload = function() {
   exibirResultado();
 };
+
+
+function interpretarIMC(imc) {
+  var divResultado = document.getElementById("resultado");
+  // Interpreta o IMC
+  if (imc < 18.5) {
+    divResultado.innerHTML = "Abaixo do peso" ;
+  } else if (imc >= 18.5 && imc < 25) {
+    divResultado.innerHTML = "Peso normal" ;
+  } else if (imc >= 25 && imc < 30) {
+    divResultado.innerHTML = "Sobrepeso" ;
+  } else {
+    divResultado.innerHTML = "Obesidade" ;
+  }
+}
+
