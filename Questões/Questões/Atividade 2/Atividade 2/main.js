a93864ed7bdb0acdf6dcf79e1d4f106158f2f798
João Adriano <95387611+joaoadriano2005@users.noreply.github.com>
2023-03-14 11:30:02 -0300

    Update main.js

--- Questões/Questões/Atividade 2/Atividade 2/main.js
+++ Questões/Questões/Atividade 2/Atividade 2/main.js
@@ -3,7 +3,20 @@ var resultado = 0;
 // Função para exibir o resultado na tela
 function exibirResultado() {
   var divResultado = document.getElementById("resultado");
-  divResultado.innerHTML = "Seu IMC é: "+resultado ;
+  divResultado.innerHTML = "" ;
+  function interpretarIMC(imc) {
+  // Interpreta o IMC
+  if (imc < 18.5) {
+    divResultado.innerHTML = "" ;
+  } else if (imc >= 18.5 && imc < 25) {
+     divResultado.innerHTML = "" ;
+  } else if (imc >= 25 && imc < 30) {
+     divResultado.innerHTML = "" ;
+  } else {
+    divResultado.innerHTML = "" ;
+  }
+}
+
 }
 
 // Função para realizar o cálculo
@@ -13,8 +26,8 @@ function calcular(kg,alt) {
   var kg = document.getElementById("input2").value;
 
   alt=alt/100
-  resultado = kg/(alt*alt)
-  resultado=resultado.toFixed(1)
+  imc = kg/(alt*alt)
+  imc=imc.toFixed(2)
   exibirResultado();
 }
 
